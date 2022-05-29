f1f53996d513f1eee878ada19ae5502563b90e01
Fajar <fajarmh2@gmail.com>
2022-05-29 21:27:47 +0700

    Update dependencies and framework esp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,16 @@ String wifiSSID = "WULAN";
 String wifiPassword = "21mei2002";
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Connecting to Wifi!");
+  WiFi.begin(wifiSSID.c_str(), wifiPassword.c_str());
+
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.println(".");
+    delay(500);
+  }
+  Serial.println("Wifi Connnected!");
+
 }
 
 void loop() {
