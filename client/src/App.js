f528fd652f473b50159c1d02cd17d3e77d4c4c14
Fajar <fajarmh2@gmail.com>
2022-06-02 00:08:23 +0700

    Add frontend real-time change data

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 function App() {
+  const [jarak, setJarak] = useState("0");
+
+  
+  useInterval(() => {
+      const rand = Math.floor(Math.random() * 10);
+      setJarak(rand);
+  }, 2000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container text-center mt-5'>
+      {jarak}
     </div>
   );
 }
